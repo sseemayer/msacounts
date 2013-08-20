3dcd846124d85e78b9349c95617243e5bda16a27
Stefan Seemayer <stefan@seemayer.de>
2013-08-20 16:37:43 +0200

    Read MSA to index matrix by default

--- msacounts/__init__.py
+++ msacounts/__init__.py
@@ -1,12 +1,15 @@
 import msacounts.cext.msacounts as msac
 import numpy as np
 
-def read_msa(f):
+def read_msa(f, return_indices=True):
 
     with open(f, 'r') as o:
         msa = o.readlines()
 
     msa = np.array([[ord(c) for c in x.strip()] for x in msa], dtype=np.uint8)
+
+    if return_indices:
+        index_msa(msa, in_place=True)
 
     return msa
 
@@ -15,8 +18,12 @@ def count(msa):
     counts = msac.msa_count(ncol * ncol * 21 * 21, msa)
     return np.reshape(counts, (ncol, ncol, 21, 21))
 
+def index_msa(msa, in_place=False):
+    if in_place:
+        msai = msa
+    else:
+        msai = msa.copy()
 
-def index_msa(msa):
-    msai = msa.copy()
     msac.msa_char_to_index(msai)
     return msai
+
